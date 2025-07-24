d5bc43460da06ff8673befdffee3f2189e119e67
Shaseram <107533892+Shaseram@users.noreply.github.com>
2025-07-24 19:55:13 -0400

    base inicial

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+Calculadora en pantalla con HTML, JS y CSS
+
+* Funciones a implementar
+- Suma 
+- Resta
+- Multiplicacion
+- Division
+
+*/
+
+const suma = (numero1, numero2) => {
+    return numero1 + numero2;
+}
+
+const resta = (numero1, numero2) => {
+    return numero1 - numero2;
+}
+
+const multiplicacion = (numero1, numero2) => {
+    return numero1 * numero2;
+}
+
+const division = (numero1, numero2) => {
+    return numero1 / numero2;
+}
+
+
+
+
+
