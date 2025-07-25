7c9c5c50103dd57991a0bf7f979e76e878246c2c
Shaseram <107533892+Shaseram@users.noreply.github.com>
2025-07-25 00:27:29 -0400

    avance visual y del script

--- script.js
+++ script.js
@@ -25,6 +25,32 @@ const division = (numero1, numero2) => {
     return numero1 / numero2;
 }
 
+const operator = (operador, numero1, numero2) => {
+    let valor = 0
+    switch(operador) {
+        case "+":
+            valor = suma(numero1, numero2)
+
+            break;
+        case "-":
+            valor = resta(numero1, numero2)
+
+            break;
+        case "X":
+            valor = multiplicacion(numero1, numero2)
+
+            break;
+        case "/":
+            valor = division(numero1, numero2)
+
+            break;
+    }
+
+    return valor;
+}
+
+const display = document.getElementById("result");
+
 
 
 
