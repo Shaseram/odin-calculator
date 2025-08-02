b092c363d28689f8ba55aa6abf34f77c611c3019
Shaseram <107533892+Shaseram@users.noreply.github.com>
2025-08-01 22:51:00 -0400

    se agrega script funcional

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const operator = (operador, numero1, numero2) => {
             valor = resta(numero1, numero2)
 
             break;
-        case "X":
+        case "x":
             valor = multiplicacion(numero1, numero2)
 
             break;
@@ -48,10 +48,62 @@ const operator = (operador, numero1, numero2) => {
 
     return valor;
 }
+let n1 = ""
+let n2 = ""
+let op = ""
 
 const display = document.getElementById("result");
 
+let numeros_array = [...document.querySelectorAll(".numero")]
+numeros_array.forEach((boton) => {
+    
+    boton.addEventListener("click", (e) => {
+        // Caso pa borrar el 0 inicial al darle click a un numero:v
 
+        let numero = boton.textContent
+        if(display.textContent === "0") { display.textContent = ""}
 
+        if(n2 == "" && op == "") {
+            display.textContent += numero
+            n1 = display.textContent
+            console.log(n1)
 
+        } else if(n1 !== "" && op !== "") {
+            // Display dejar en blanco en el event listener de los operaadores
+            if(n2 === "") {
+                display.textContent = ""
+            }
+            display.textContent += numero
+            n2 = display.textContent
+            console.log(n2)
+        }
+        
+    })
+})
 
+let operadores_array = [...document.querySelectorAll(".operador")]
+
+operadores_array.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+        let operador = boton.textContent
+        // Caso donde se aprete un operador sin apretar el igual (=)
+        if(n1 !== "" && n2 !== "") {
+            display.textContent = operator(op, Number(n1), Number(n2))
+            n1 = display.textContent
+            n2 = ""
+            op = operador
+            console.log("Entraste en el caso" + op)
+        } else {
+            op = operador
+            console.log(op)
+        }
+    })
+})
+
+const equal = document.getElementById("igual");
+
+equal.addEventListener("click", () => {
+    display.textContent = operator(op,Number(n1), Number(n2))
+    
+
+})
